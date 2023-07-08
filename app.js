f626a2ec78fea148a4217e9a4af73969d2b570b1
Ivan Stremaus <i.stremaus@yandex.ru>
2023-07-09 01:58:29 +0600

    fix: change process.env

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const router = require('./routes/index');
 
 // const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT, BASE_PATH } = process.env;
+// const { PORT, BASE_PATH } = process.env;
+const { PORT = 3000, BASE_PATH = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
